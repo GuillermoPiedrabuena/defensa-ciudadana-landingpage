a6c9262cdbc2ea9e6f113f8adf0fd7583a5526a6
Guillermo Piedrabuena <gnpiedrabuena@defensa-ciudadana.com>
2024-12-21 00:24:09 -0300

    saving

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+
+app = FastAPI()
+
+# Define las rutas estáticas y los templates
+STATIC_DIR = Path(__file__).parent / "statics"
+TEMPLATE_DIR = Path(__file__).parent / "templates"
+
+app.mount("/statics", StaticFiles(directory=STATIC_DIR), name="statics")
+templates = Jinja2Templates(directory=STATIC_DIR)
+
+@app.get("/herencias", response_class=HTMLResponse)
+async def get_herencias(request: Request):
+    return templates.TemplateResponse("herencias.html", {"request": request, "logo_url": "/statics/logo.png"})
+
+@app.get("/alimentos", response_class=HTMLResponse)
+async def get_alimentos(request: Request):
+    return templates.TemplateResponse("alimentos.html", {"request": request, "logo_url": "/statics/logo.png"})
